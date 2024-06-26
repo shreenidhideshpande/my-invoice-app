346350e4508742a01122fcb081c5a186ad871574
shreenidhi deshpande <shreenidhideshpande4u@gmail.com>
2024-06-26 17:30:20 +0530

    changed homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,12 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal `}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to Acme.</strong> An invoicing application built
+            using Next.js
+            {/* <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought to you by Vercel. */}
           </p>
           <Link
             href="/dashboard"
